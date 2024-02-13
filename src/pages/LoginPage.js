28c0a3b74ac1b3ef77f302d91483cc44559d54f1
Vishvadeep <vishvadeep.0001@gmail.com>
2024-02-13 22:14:32 +0530

    using react-router routes has been created

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Login} from "../features/auth/components/Login"
+
+function LoginPage() {
+  return (
+    <Login/>
+  )
+}
+
+export default LoginPage
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Signup } from '../features/auth/components/Signup'
+
+function SignupPage() {
+  return (
+    <Signup/>
+  )
+}
+
+export default SignupPage
