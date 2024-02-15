ca7a006755ede57b53996e6f2aa2e59aed662a9c
Vishvadeep <vishvadeep.0001@gmail.com>
2024-02-15 22:42:12 +0530

    Checkout page completed

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
   Link,
 } from "react-router-dom";
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
 
 
 const router = createBrowserRouter([
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage/>
+  },
+  {
+    path: "/checkout",
+    element: <Checkout/>
   },
 ]);
 
